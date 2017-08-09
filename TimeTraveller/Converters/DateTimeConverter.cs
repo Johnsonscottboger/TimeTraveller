a597b412fa31286c35667219755474921503e8f3
Aning <a937515012@live.com>
2017-08-09 23:34:48 +0800

    修改-完成时钟和主界面的整合

---
+++ TimeTraveller/Converters/DateTimeConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TimeTraveller.Converters
+{
+    public class DateTimeConverter :IValueConverter
+    {
+        public Object Convert(Object value,Type targetType,Object parameter,CultureInfo culture)
+        {
+            DateTime dt;
+            try
+            {
+                dt = (DateTime)value;
+            }
+            catch
+            {
+                dt = new DateTime();
+            }
+            return dt.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        public Object ConvertBack(Object value,Type targetType,Object parameter,CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TimeTraveller/View/Watch.xaml.cs
+++ TimeTraveller/View/Watch.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TimeTraveller.View
 {
@@ -20,9 +22,36 @@ namespace TimeTraveller.View
     /// </summary>
     public partial class Watch : UserControl
     {
+        /// <summary>
+        /// 当前时间
+        /// </summary>
+        public DateTime CurrentTime
+        {
+            get { return (DateTime)GetValue(CurrentTimeProperty); }
+            set { SetValue(CurrentTimeProperty,value); }
+        }
+
+
+        public static readonly DependencyProperty CurrentTimeProperty =
+            DependencyProperty.Register("CurrentTime",typeof(DateTime),typeof(Watch),new PropertyMetadata(DateTime.Now));
+
+
+        public DispatcherTimer MainTimer { get; set; }
+
         public Watch()
         {
             InitializeComponent();
+
+            this.MainTimer = new DispatcherTimer();
+            this.MainTimer.Tick += MainTimer_Tick;
+            this.MainTimer.Interval = TimeSpan.FromSeconds(1);
+            this.MainTimer.Start();
+        }
+
+        void MainTimer_Tick(object sender,EventArgs e)
+        {
+            this.CurrentTime = DateTime.Now;
+            Debug.WriteLine(this.CurrentTime);
         }
     }
 }
